ce79c2c99177d3cdc3d7e26d35feb9c6a008e1c0
miyanew <dev38_shin@yahoo.co.jp>
2024-08-18 00:51:58 +0900

    macのcalコマンドで1ヶ月分のみ実装

--- 02.calendar/calender.py
+++ 02.calendar/calender.py
@@ -1,9 +1,24 @@
 import argparse
+import calendar
+import re
 from datetime import datetime
 
 
-def main(options):
-    print(options)
+def main(opts):
+    cal = calendar.LocaleTextCalendar(
+        firstweekday=calendar.SUNDAY,
+        locale="ja_JP.UTF-8",
+    )
+    cal_str = cal.formatmonth(opts.year, opts.month)
+    cal_formatted = format_weeday_line(cal_str)
+    print(cal_formatted)
+
+
+def format_weeday_line(calendar_str):
+    weekday_pattern = r"^.*日.*月.*火.*水.*木.*金.*土.*$"
+    weekday_line = re.search(weekday_pattern, calendar_str, re.MULTILINE).group()
+    new_weekday_line = "日 月 火 水 木 金 土"
+    return calendar_str.replace(weekday_line, new_weekday_line)
 
 
 if __name__ == "__main__":
